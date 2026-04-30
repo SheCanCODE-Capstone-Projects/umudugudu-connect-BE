225658decc1501503a2cc3414a957669afaf2045
nishimwecelie <nishimwecelie@gmail.com>
2026-04-30 15:43:12 +0200

    added role in verify-token

--- src/main/java/com/umudugudu/service/impl/AuthServiceImpl.java
+++ src/main/java/com/umudugudu/service/impl/AuthServiceImpl.java
@@ -98,7 +98,11 @@ public class AuthServiceImpl implements AuthService {
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         UserDetails userDetails = new org.springframework.security.core.userdetails.User(
-                user.getPhoneNumber(), "", List.of()
+                user.getPhoneNumber(),
+                "",
+                List.of(new org.springframework.security.core.authority.SimpleGrantedAuthority(
+                        "ROLE_" + user.getRole().name()
+                ))
         );
 
         if (!jwtUtils.isTokenValid(refreshToken, userDetails)) {
