cfbf7369618b495361c82b45dc9ac4dff10d9676
nishimwecelie <nishimwecelie@gmail.com>
2026-04-30 11:28:50 +0200

    added otp-entity

---
+++ src/main/java/com/umudugudu/entity/Otp.java
@@ -0,0 +1,20 @@
+package com.umudugudu.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Otp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String phoneNumber;
+    private String code;
+    private LocalDateTime expiryTime;
+    private int attempts;
+}
