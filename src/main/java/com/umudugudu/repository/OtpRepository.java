6bd8296a4d654f84bbdde2419d665606fddad166
nishimwecelie <nishimwecelie@gmail.com>
2026-05-06 12:37:42 +0200

    added @Transational annotation on jpa repo to help remove existing otps which are expired

--- src/main/java/com/umudugudu/repository/OtpRepository.java
+++ src/main/java/com/umudugudu/repository/OtpRepository.java
@@ -2,6 +2,7 @@ package com.umudugudu.repository;
 
 import com.umudugudu.entity.Otp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,5 +11,9 @@ public interface OtpRepository extends JpaRepository<Otp, Long> {
     Optional<Otp> findTopByPhoneNumberOrderByIdDesc(String phoneNumber);
     Optional<Otp> findTopByEmailOrderByIdDesc(String email);
 
+    @Transactional
+    void deleteByPhoneNumber(String phoneNumber);
+
+    @Transactional
     void deleteByEmail(String email);
 }
