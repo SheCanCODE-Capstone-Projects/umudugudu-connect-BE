8589051e79076d639be3174f5dba1295203bf398
nishimwecelie <nishimwecelie@gmail.com>
2026-05-11 13:20:39 +0200

    added attendance service interfacce

---
+++ src/main/java/com/umudugudu/service/AttendanceService.java
@@ -0,0 +1,16 @@
+package com.umudugudu.service;
+
+import com.umudugudu.dto.request.BulkAttendanceSyncRequest;
+import com.umudugudu.dto.request.MarkAttendanceRequest;
+import com.umudugudu.dto.response.AttendanceResponse;
+import com.umudugudu.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AttendanceService {
+    AttendanceResponse markAttendance(UUID activityId, MarkAttendanceRequest request, User isiboLeader);
+    List<AttendanceResponse> syncOfflineAttendance(BulkAttendanceSyncRequest request, User isiboLeader);
+    List<AttendanceResponse> getAttendanceForActivity(UUID activityId);
+    List<AttendanceResponse> getAbsentMembers(UUID activityId);
+}
