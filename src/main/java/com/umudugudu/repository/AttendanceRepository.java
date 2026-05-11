11866f3802e0863be253cbb7970625c582c38aaf
nishimwecelie <nishimwecelie@gmail.com>
2026-05-11 13:56:15 +0200

    added new method in attendance repo

--- src/main/java/com/umudugudu/repository/AttendanceRepository.java
+++ src/main/java/com/umudugudu/repository/AttendanceRepository.java
@@ -16,4 +16,6 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     List<Attendance> findByActivityIdAndStatus(UUID activityId, AttendanceStatus status);
     Optional<Attendance> findByActivityIdAndCitizenId(UUID activityId, Long citizenId);
     List<Attendance> findByCitizenId(Long citizenId);
+    List<Attendance> findByCitizen_Id(Long citizenId);
+    List<Attendance> findByCitizen_Isibo_Id(UUID isiboId);
 }
