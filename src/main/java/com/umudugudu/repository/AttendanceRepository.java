5b8daf3a24262619514eaa7ac086fda84383a788
nishimwecelie <nishimwecelie@gmail.com>
2026-05-11 13:06:40 +0200

    added attendance repo

---
+++ src/main/java/com/umudugudu/repository/AttendanceRepository.java
@@ -0,0 +1,19 @@
+package com.umudugudu.repository;
+
+import com.umudugudu.entity.Attendance;
+import com.umudugudu.entity.AttendanceStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+
+    List<Attendance> findByActivityId(UUID activityId);
+    List<Attendance> findByActivityIdAndStatus(UUID activityId, AttendanceStatus status);
+    Optional<Attendance> findByActivityIdAndCitizenId(UUID activityId, Long citizenId);
+    List<Attendance> findByCitizenId(Long citizenId);
+}
