103c702cdbb5645b76ee562c16bffe14ee344a43
nishimwecelie <nishimwecelie@gmail.com>
2026-05-25 11:35:04 +0200

    attendance status modified

--- src/main/java/com/umudugudu/entity/AttendanceStatus.java
+++ src/main/java/com/umudugudu/entity/AttendanceStatus.java
@@ -2,10 +2,6 @@ package com.umudugudu.entity;
 
 public enum AttendanceStatus {
     PRESENT,
-<<<<<<< HEAD
-    ABSENT
-=======
     ABSENT,
     EXCUSED
->>>>>>> be3e460 (E3.1 assign penalty to absent citizen)
 }
