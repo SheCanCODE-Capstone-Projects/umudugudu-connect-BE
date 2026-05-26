c76f7be17e232a0a745c53c5c7da27a42089ac17
nishimwecelie <nishimwecelie@gmail.com>
2026-05-26 20:15:43 +0200

    set explicit HTTPS server URL in Swagger to fix Railway proxy detection

--- src/main/java/com/umudugudu/config/OpenApiConfig.java
+++ src/main/java/com/umudugudu/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,9 @@ public class OpenApiConfig {
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
+                .addServersItem(new Server()
+                        .url("https://umudugudu-connect-be-production.up.railway.app")
+                        .description("Production server"))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
